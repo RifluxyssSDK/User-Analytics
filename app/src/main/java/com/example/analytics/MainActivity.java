48aeef7366d787c1195c7cc3e688ad4fb0ba56e5
Mukesh <p.mukeshkannan@rifluxyss.com>
2023-01-03 18:08:11 +0530

    DAO Updated

--- app/src/main/java/com/example/analytics/MainActivity.java
+++ app/src/main/java/com/example/analytics/MainActivity.java
@@ -20,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
             Analytics.insertLog(new Schema("userID","hostID","locationNumber",100.8957,"log","eventNBR","assDES",123));
         }
 
-        Log.i("TAG", "onCreate: ");
+
     }
 }
